907c77312f71b50a9f73c2ae2b4f0b1040c50881
Gina <280791931@qq.com>
2020-02-18 13:04:26 +0800

    Base code

---
+++ AppDemoForIOS/AppDemoForIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppDemoForIOS
+//
+//  Created by GuoLiang on 2020/2/18.
+//  Copyright © 2020 scan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
